6d4230be6d272df5e7217de280362e02db16323f
Sergei Korchemkin <hellk.pub@gmail.com>
2020-02-05 15:07:07 +0300

    Create fully responsive layout for all game steps

--- rock-paper-scissors-master/main.js
+++ rock-paper-scissors-master/main.js
@@ -2,12 +2,12 @@ var btnRules = document.querySelector('#js-btn-rules')
 var btnClose = document.querySelector('#js-btn-rules-modal-close')
 var modalRules = document.querySelector('#js-rules-modal')
 
-btnRules.addEventListener('click', function openRulesModal(e) {
+btnRules.onclick = function openRulesModal(e) {
   e.preventDefault();
   modalRules.style.visibility = 'visible'
-})
+}
 
-btnClose.addEventListener('click', function closeRulesModal(e) {
+btnClose.onclick = function closeRulesModal(e) {
   e.preventDefault();
   modalRules.style.visibility = 'hidden'
-})
+}
